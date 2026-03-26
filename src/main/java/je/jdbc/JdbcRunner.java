48d926e19a7053bfbdead35cdeabb544abfaf833
Mikeysoll <contact@mikeysoll.ru>
2026-03-26 14:16:10 +0300

    Реализовал connection pull

--- src/main/java/je/jdbc/JdbcRunner.java
+++ src/main/java/je/jdbc/JdbcRunner.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
-        try (var connection = ConnetionManager.open()) {
+        try (var connection = ConnetionManager.get()) {
             System.out.println(connection.getTransactionIsolation());
         }
     }
--- src/main/java/je/jdbc/utils/ConnetionManager.java
+++ src/main/java/je/jdbc/utils/ConnetionManager.java
@@ -1,16 +1,52 @@
 package je.jdbc.utils;
 
+import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public final class ConnetionManager {
 
     public static final String URL_KEY = "db.url";
     public static final String USER_KEY = "db.user";
     public static final String PASSWORD_KEY = "db.password";
+    public static final int DEFAULT_POOL_SIZE = 10;
+    public static final String POOL_SIZE_KEY = "db.pool.size";
+    private static BlockingQueue<Connection> pool;
 
-    public static Connection open() {
+    static {
+        initConnectionPool();
+    }
+
+    private static void initConnectionPool() {
+        String poolSize = PropertiesUtil.get(POOL_SIZE_KEY);
+
+        int size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
+        pool = new ArrayBlockingQueue(size);
+
+        for (int i = 0; i < size; i++) {
+            Connection connection = open();
+            var proxyConnetion = (Connection) Proxy.newProxyInstance(ConnetionManager.class.getClassLoader(),
+                    new Class[]{Connection.class},
+                    (proxy, method, args) ->
+                            method.getName().equals("close") ?
+                                    pool.add((Connection) proxy) :
+                                    method.invoke(connection, args));
+            pool.add(proxyConnetion);
+        }
+    }
+
+    public static Connection get() {
+        try {
+            return pool.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Connection open() {
         try {
             return DriverManager.getConnection(
                     PropertiesUtil.get(URL_KEY),
